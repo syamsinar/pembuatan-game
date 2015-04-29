ead6ba14df53ff642c340397a7acf356debfb6db
syamsinar <adry.indah@gmail.com>
2015-04-29 12:47:09 +0800

    sinar

---
+++ Assets/control.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class control : MonoBehaviour {
+	public float longjump;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GetComponent<Rigidbody>().velocity = Vector3.zero;
+			GetComponent<Rigidbody>().AddForce (Vector3.up * longjump);
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			transform.position += (Vector3.right / 10);
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			transform.position += (Vector3.left / 10);
+		}
+		
+	}
+}
